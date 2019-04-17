d8aa150728351cf21f123e139ca2ab7c8919e149
nico <brun.nicolas21@gmail.com>
2019-04-17 10:31:21 +0200

    command take|merge skeleton

---
+++ cmd/vmucat/sort.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "github.com/midbel/cli"
+)
+
+func runMerge(cmd *cli.Command, args []string) error {
+  return nil
+}
+
+func runTake(cmd *cli.Command, args []string) error {
+  return nil
+}
