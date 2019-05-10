40805a344c5d171944005cddc7494f8ed4e4dddc
nico <brun.nicolas21@gmail.com>
2019-05-10 10:37:24 +0200

    adjust columns width

--- dump.go
+++ dump.go
@@ -94,19 +94,19 @@ func (d *Dumper) dumpPacket(p Packet, valid bool) {
 	// packet VMU info
 	d.line.AppendTime(v.Timestamp(), rt.TimeFormat, linewriter.AlignCenter)
 	d.line.AppendUint(uint64(v.Sequence), 7, linewriter.AlignRight)
-	d.line.AppendUint(uint64(diff), 3, linewriter.AlignRight)
+	d.line.AppendUint(uint64(diff), 5, linewriter.AlignRight)
 	d.line.AppendBytes(WhichMode(p.IsRealtime()), 8, linewriter.AlignCenter|linewriter.Text)
 	d.line.AppendBytes(WhichChannel(v.Channel), 4, linewriter.AlignCenter|linewriter.Text)
 	// packet HRD info
 	d.line.AppendUint(uint64(c.Origin), 2, linewriter.AlignRight|linewriter.Hex|linewriter.WithZero)
 	d.line.AppendTime(c.Acquisition(), rt.TimeFormat, linewriter.AlignCenter)
 	d.line.AppendUint(uint64(c.Counter), 8, linewriter.AlignRight)
-	d.line.AppendBytes(c.UserInfo(), 16, linewriter.AlignLeft|linewriter.Text)
+	d.line.AppendBytes(c.UserInfo(), 14, linewriter.AlignLeft|linewriter.Text)
 	d.line.AppendString(p.DataType(), 6, linewriter.AlignRight)
 	// d.line.AppendString(p.String(), 64, linewriter.AlignLeft)
 	// packet sums and validity state
 	d.line.AppendUint(uint64(p.Sum), 8, linewriter.AlignRight|linewriter.Hex|linewriter.WithZero)
-	d.line.AppendBytes(bad, 8, linewriter.AlignCenter|linewriter.Text)
+	d.line.AppendBytes(bad, 7, linewriter.AlignCenter|linewriter.Text)
 	if len(p.Data) > 0 {
 		d.line.AppendUint(xxh.Sum64(p.Data, 0), 16, linewriter.AlignRight|linewriter.Hex|linewriter.WithZero)
 	}
